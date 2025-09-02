c504a645d6f80b2974deb1f8ce47633e4ede0d19
Matthew Holliday <matthew.holliday@mastek.com>
2025-09-02 12:59:13 -0700

    for loop converted

--- src/aixml/elements/for_loop_element.py
+++ src/aixml/elements/for_loop_element.py
@@ -3,27 +3,45 @@ from ..helpers.boolean_helper import BooleanHelper
 from .base_element import BaseElement
 
 class ForLoopElement(BaseElement):
-    def __init__(self, start_num: int, stop_num: int):
+    def __init__(self, start_num: str, stop_num: str):
         self.is_repeating = True
-        self.current_iteration = start_num
-        self.stop_num = stop_num
+        
+        self.current_iteration_str = start_num
+        self.stop_num_str = stop_num
+
+        self.stop_num_int = None
+        self.current_iteration_int = None
+        
+        self.current_iteration_evaluated = ""
+        self.stop_num_evaluated = ""
 
     def enter(self, message: Message) -> Message:
+        print("current_iteration_str:", self.current_iteration_str)
+        print("stop_num_str:", self.stop_num_str)
+
+        try:
+            self.current_iteration_evaluated, self.stop_num_evaluated = self.apply_templates(templates=[self.current_iteration_str, self.stop_num_str], message=message)
+            self.current_iteration_int = int(self.current_iteration_evaluated)
+            self.stop_num_int = int(self.stop_num_evaluated)
+        except Exception as e:
+            error_message = f"ForLoopElement encountered an error during evaluation: {e}"
+            print(error_message)
+            raise e
         return message
     
     def exit(self, message: Message) -> Message:
         return message
 
     def increment_iteration(self, message: Message) -> Message:
-        self.current_iteration += 1
-        message.set_var("index", self.current_iteration)
+        self.current_iteration_int += 1
+        message.set_var("index", self.current_iteration_int)
         return message
 
     def should_exit(self, message: Message) -> tuple[bool, Message]:
         result: bool = False
         
         try:
-            result = self.current_iteration < self.stop_num
+            result = self.current_iteration_int < self.stop_num_int
             return (result == False, message) # Exit if condition is false
         except Exception as e:
-            return (True, message.log_message(f"ForLoopElement '{self.name}' encountered an error: {e}")) # Stop on error
+            return (True, message.log_message(f"ForLoopElement encountered an error: {e}")) # Stop on error
