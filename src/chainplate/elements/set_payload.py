29c64a06fc2d54bb42c29b721066de494e108159
Matthew Holliday <matthew.holliday@mastek.com>
2025-09-03 14:40:41 -0700

    added payload data

---
+++ src/chainplate/elements/set_payload.py
@@ -0,0 +1,24 @@
+from .base_element import BaseElement
+from ..message import Message
+
+class SetPayloadElement(BaseElement):
+    def __init__(self, input_var: str = "", content: str = ""):
+        self.input_var = input_var
+        self.content = content
+
+    def enter(self, message: Message) -> Message:
+        value = self.get_input_value(message, self.input_var, self.content)
+        message.set_var("__payload__", value) #TODD - consolidate magic strings into a constants file or class
+        return message
+
+    def exit(self, message: Message) -> Message:
+        return message
+    
+    def get_input_value(self, message: Message, input_var: str, content: str) -> str:
+        if input_var:
+            return message.get_var(input_var)
+        elif content:
+            return content
+        else:
+            raise ValueError("Either 'input_var' or 'content' must be provided to get the input value.")
+        
